445e18e2fd8fa828c4008f4d417329e224e24aa7
Rob Herold <rderek@bresnan.net>
2018-11-13 11:32:23 -0700

    second attempt

---
+++ SpriteUtility_111318/SpriteUtility_111318/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpriteUtility_111318
+//
+//  Created by Rob Herold on 11/13/18.
+//  Copyright © 2018 Rob Herold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
